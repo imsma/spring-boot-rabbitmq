d46b5e6f5d33e4a44a0d97884dcaad48d5f4849f
SMA <sma@wizlinx.com>
2024-11-09 05:49:45 +0500

    RPC

--- producer/src/main/java/im/sma/rabbitmq/producer/config/RabbitMQConfig.java
+++ producer/src/main/java/im/sma/rabbitmq/producer/config/RabbitMQConfig.java
@@ -1,27 +1,15 @@
 package im.sma.rabbitmq.producer.config;
 
-import org.springframework.amqp.core.AnonymousQueue;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
-    @Bean
-    public TopicExchange topicExchange() {
-        return new TopicExchange("topicExchange");
-    }
+    public static final String REQUEST_QUEU = "rpc.request.queue";
 
     @Bean
-    public Queue annQueue() {
-        return new AnonymousQueue();
-    }
-
-    @Bean
-    public Binding binding(Queue annQueue, TopicExchange topicExchange) {
-        return BindingBuilder.bind(annQueue).to(topicExchange).with("topicRouting");
+    public Queue requestQueue() {
+        return new Queue(REQUEST_QUEU);
     }
 }
